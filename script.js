958359280c693e83e9da7593e5485c54aff02697
ASIK-0 <exe.ashiqui@gmail.com>
2025-08-29 20:37:55 +0600

    call alert and coin count js added

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // heart count
 const sumNumber = 1
-const queryBtn = (document.querySelectorAll('.heart-btn'));
+const queryBtn = document.querySelectorAll('.heart-btn');
 
 for (query of queryBtn) {
     query.addEventListener('click', function () {
@@ -9,4 +9,39 @@ for (query of queryBtn) {
         document.getElementById('heart-count').innerText = sumNum;
     })
 }
+
+// card section
+// call button 
+const callHistoryData = []
+const callBtn = document.querySelectorAll('.call-button');
+const coinNum = 20
+for(const call of callBtn){
+    call.addEventListener('click', function (){
+        const card = call.parentNode.parentNode.parentNode
+        const callName = card.querySelector(".call-name").innerText;
+        const callNumber = card.querySelector(".call-number").innerText;
+
+        const coinCountEl = document.getElementById('coin-count')
+        let count = parseInt(coinCountEl.innerText)
+
+        if(count <= 0 ){
+            alert("⚠️You don't have any money! 20 money to call.")
+            return
+        }
+        coins = count - coinNum
+        document.getElementById('coin-count').innerText = coins
+
+
+        alert('📞calling ' + callName + ' ' + callNumber +' '+ ' ...')
+        const data = {
+        naem: callName,
+        num: callNumber,
+        date: new Date().toLocaleTimeString()
+    }
+     callHistoryData.push(data)
+     console.log(callHistoryData)
+    })   
+}
+
+// Call history
 
